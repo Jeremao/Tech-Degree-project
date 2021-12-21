aa38ad4a44bb4b3a373ae42fbc2f64fa9f8599a4
Jeremao <93031340+Jeremao@users.noreply.github.com>
2021-12-21 14:06:54 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,35 +1,35 @@
 
 const quotes = [ // Array of objects
 {
-  text: 'A journey of a thousand miles begins with a single step.',
-  names: 'Lao Tzu',
+  quote: 'A journey of a thousand miles begins with a single step.',
+  source: 'Lao Tzu',
   citation: 'Dao De Jing'
 },
 {
-  text: 'Religion is excellent stuff for keeping common people quiet. Religion is what keeps the poor from murdering the rich.',
-  names: 'Napoleon Bonaparte',
+  quote: 'Religion is excellent stuff for keeping common people quiet. Religion is what keeps the poor from murdering the rich.',
+  source: 'Napoleon Bonaparte',
   citation: 'Meeting of the National Committee',
   year: 1806
 },
 {
-  text: 'Im Batman.',
-  names: 'Batman'
+  quote: 'Im Batman.',
+  source: 'Batman'
 },
 {
-  text: 'The philosophers have only interpreted the world, in various ways. The point, however, is to change it.',
-  names: 'Karl Marx',
+  quote: 'The philosophers have only interpreted the world, in various ways. The point, however, is to change it.',
+  source: 'Karl Marx',
   citation: 'Eleven Theses on Feuerbach',
   year: 1888
 },
 {
-  text: 'Those who dare to fail miserably can achieve greatly.',
-  names: 'John F. Kennedy',
+  quote: 'Those who dare to fail miserably can achieve greatly.',
+  source: 'John F. Kennedy',
   citation: 'University of Capetown, Capetown, South Africa',
   year: 1966
 },
 {
-  text: 'Life is like a box of chocolates. You never know what you’re going to get.',
-  names: 'Forest Gump',
+  quote: 'Life is like a box of chocolates. You never know what you’re going to get.',
+  source: 'Forest Gump',
   year: 1994
 }
 ];
@@ -37,7 +37,7 @@ const quotes = [ // Array of objects
 //Gets a random number along the length of the quote array
 
 function getRandomQuote (arr) {
-let randomQuote = Math.floor(Math.random() * arr.length) +1;
+let randomQuote = Math.floor(Math.random() * arr.length);
  return arr[randomQuote];
 }
 
@@ -48,8 +48,8 @@ If there is a citation or year the quote will add to the html
 
 function printQuote () {
   let words = getRandomQuote(quotes);
-  let html = `<p class="quote"> ${words.text} </p>`;
-  html += `<p class="source"> ${words.names} </p>`;
+  let html = `<p class="quote"> ${words.quote} </p>`;
+  html += `<p class="source"> ${words.source} </p>`;
   
 if("citation" in words) {
 html += `<span class="citation"> ${words.citation} </span>`;
